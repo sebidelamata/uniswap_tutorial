021f862b5bd01ba5c38de5b8b847e0a3a8acbb25
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2023-03-05 11:57:22 -0700

    made changes

--- index.js
+++ index.js
@@ -42,39 +42,79 @@ const init = async() => {
         TradeType.EXACT_INPUT
     );
     // find the price with 6 sigfigs dai per eth (input token weth gives dai)
-    console.log("WETH Price in DAI: " + route.midPrice.toSignificant(6));
+    console.log(`WETH Price in DAI: ${route.midPrice.toSignificant(6)}`);
     // to find the opposite eth per dai
-    console.log("1 DAI = " + route.midPrice.invert().toSignificant(6)) + " WETH";
+    console.log(`1 DAI =  ${route.midPrice.invert().toSignificant(6)}`) + " WETH";
     // log the execution price for this trade
     console.log(trade.executionPrice.toSignificant(6));
     // find the next price after this trade (due to bonding curve)
     console.log(trade.nextMidPrice.toSignificant(6));
 
-    // define slippage tolerance as 50 bips
-    slippageTolerance = new Percent('50', '10000');
 
-    // define the minimum amount we want to buy
-    const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw;
 
-    // path of our trade
-    const path = [weth.address, dai.address];
+    // here we show how we would execute a trade of  eth for dai
+    // // define slippage tolerance as 50 bips
+    // slippageTolerance = new Percent('50', '10000');
 
-    const to = "";
+    // // define the minimum amount we want to buy
+    // const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw;
 
-    // set a deadline for our trade for 20 minutes
-    const deadline = Math.floor(Date.now() / 1000 * 20 * 60);
+    // // path of our trade
+    // const path = [weth.address, dai.address];
 
-    // how much are we going to send (input)
-    const value = trade.inputAmount.raw
+    // const to = "";
 
-    // create provider
-    const provider = ethers.getDefaultProvider("mainnet", {
-        infura: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`
-    });
+    // // set a deadline for our trade for 20 minutes
+    // const deadline = Math.floor(Date.now() / 1000 * 20 * 60);
 
-    const signer = new ethers.Wallet(PRIVATE_KEY);
+    // // how much are we going to send (input)
+    // const value = trade.inputAmount.raw
+
+    // // create provider
+    // const provider = ethers.getDefaultProvider("mainnet", {
+    //     infura: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`
+    // });
+
+    // // create signer object with our provider and credentials
+    // const signer = new ethers.Wallet(process.env.PRIVATE_KEY);
+    // const account = signer.connect(provider);
+
+    // // represent smart contract of uniswap
+    // const uniswap = new ethers.Contract(
+    //     // v2 router contract
+    //     '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+    //     // abi for functions we need
+    //     [
+    //         'function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);'
+    //     ],
+    //     // account info
+    //     account
+    // )
+
+    // // create transaction
+    // const tx = await uniswap.swapExactETHforTokens(
+    //     amountOutMin,
+    //     path,
+    //     to,
+    //     deadline,
+    //     {
+    //         value: value,
+    //         gasPrice: 20e9
+
+    //     }
+    // );
+
+    // console.log(`Transaction Hash: ${tx.hash}`);
+
+    // const receipt = await tx.wait();
+
+    // console.log(`Transaction was mined on block ${receipt.blockNumber}`);
 
 }
+
+
+
+
 
 init();
 
